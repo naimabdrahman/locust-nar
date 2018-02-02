3cd17d108b1e57c3ced2b8e0b12d70692e2d36b4
root <root@localhost.localdomain>
2018-02-02 17:22:57 +0800

    Fri Feb  2 17:22:57 +08 2018

---
+++ install-locust.sh
@@ -0,0 +1,12 @@
+# install locust on centos 7 (python 2.7)
+
+# installation 
+yum -y install python python-pip
+sleep 2
+pip install locustio
+sleep 2
+
+# run locust
+locust --help # with help menu
+
+
